cad665c9251c05ea88806ed26562b76b603943c1
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-04-30 01:04:56 -0700

    Update schema.js

--- API/schema.js
+++ API/schema.js
@@ -179,7 +179,7 @@ const typeDefs = gql`
     contactUser(input: ContactUser): String
     
     #getLoggedInUsername(testInput: String): String #a query which can be used to get the username of the logged in user
-    getLoggedInUsername: String
+    getLoggedInUsername(noInput: String): String
 
   }
 `;
