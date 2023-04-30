918cbc7d1a5de13010c9e5f9a1e103a028248b7a
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-04-29 22:33:57 -0700

    Update apollo-client.ts

--- frontend/src/apollo-client.ts
+++ frontend/src/apollo-client.ts
@@ -33,7 +33,7 @@ const delay = setContext(
 
 const cache = new InMemoryCache();
 const http = new HttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: "ambrosia-garden.vercel.app/saveRecipe",
 });
 
 const link = ApolloLink.from([delay, http]);
