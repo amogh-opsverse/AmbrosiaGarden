57d4bedada0b326a2c05967a1b35e545bdf5c584
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-05-07 08:18:45 -0700

    Update SearchFilter.tsx

--- frontend/src/components/SearchFilter.tsx
+++ frontend/src/components/SearchFilter.tsx
@@ -166,7 +166,7 @@ const SearchFilter: React.FC<SearchFilterProps> = ({
       <div>
         <button
           onClick={handleSearchClick}
-          className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          className="mt-6 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
         >
           Show Results
         </button>
